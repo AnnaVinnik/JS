2f0d1519cdd6953b47900b0ea345d53512927380
Anna <vinnik.anna@rambler.ru>
2022-07-31 18:28:38 +0700

    Созданы каталог и фильтрация

--- script.js
+++ script.js
@@ -1,178 +1,250 @@
 "use strict";
 
-const people = [
-    {name: "Anna", profession: "programmer"},
-    {name: "Irina", profession: "accounter"},
-    {name: "Olga", profession: "psytherapist"},
-    {name: "Anastasia", profession: "programmer"},
+const products = [
+    {name: "букет 1", type: "букет", price: 1000, img: "4.jpg"},
+    {name: "букет 2", type: "букет", price: 1500, img: "2.jpg"},
+    {name: "белые розы", type: "розы", price: 1000, img: "1.png"},
+    {name: "букет 3", type: "букет", price: 1200, img: "3.jpg"},
+    {name: "белые розы", type: "розы", price: 1000, img: "1.png"},
 ];
 
-// const people = ["Anna", "Irina", "Olga", "Anastasia"];
+const catalog = document.querySelector(".catalog");
 
-const header = document.getElementById("header");
-console.dir(header);
-let counterClicks = 0;
-const card = document.querySelector(".card");
+// let cardProduct = document.createElement('div');
+// cardProduct.classList = "card";
 
-card.onclick = () => {
-    card.style.transition = "2s";
-    if (counterClicks % 2 === 0) {
-        card.style.transform = "rotateY(180deg)";
-    } else {
-        card.style.transform = "rotateY(360deg)";
-    }
-    counterClicks++;
+// catalog.prepend(cardProduct);
+// cardProduct.style.backgroundColor ="red";
+products.forEach ( (product) =>  {
+    let cardProduct = document.createElement('div');
+
+    cardProduct.classList = "card";
+    cardProduct.innerHTML = `<img src=${product.img} class="card__img"></img>`;
+    
+    let productName = document.createElement('a');
+    productName.innerText = product.name;
+    productName.classList = "card__name";
+    productName.href = "#";
+
+    let productPrice = document.createElement('p');
+    productPrice.classList = "card__price";
+    productPrice.innerText = `${product.price} руб.`;
+
+    cardProduct.append(productName);
+    cardProduct.append(productPrice);
+
+    catalog.append(cardProduct);
+});
+
+
+function filter() {
+    const choosenProducts = products.filter( (product) => product.type === "букет");
+    console.log(choosenProducts);
+    
+    const elementsToRemove = document.querySelectorAll(".card");
+    elementsToRemove.forEach( (element) => {
+        element.remove();
+    });
+
+
+    choosenProducts.forEach ( (product) =>  {
+        let cardProduct = document.createElement('div');
+    
+        cardProduct.classList = "card";
+        cardProduct.innerHTML = `<img src=${product.img} class="card__img"></img>`;
+        
+        let productName = document.createElement('a');
+        productName.innerText = product.name;
+        productName.classList = "card__name";
+        productName.href = "#";
+    
+        let productPrice = document.createElement('p');
+        productPrice.classList = "card__price";
+        productPrice.innerText = `${product.price} руб.`;
+    
+        cardProduct.append(productName);
+        cardProduct.append(productPrice);
+    
+        catalog.append(cardProduct);
+    });
+}
+
+
+// const people = [
+//     {name: "Anna", profession: "programmer"},
+//     {name: "Irina", profession: "accounter"},
+//     {name: "Olga", profession: "psytherapist"},
+//     {name: "Anastasia", profession: "programmer"},
+// ];
+
+// // const people = ["Anna", "Irina", "Olga", "Anastasia"];
+
+// const header = document.getElementById("header");
+// console.dir(header);
+// let counterClicks = 0;
+// const card__rotate = document.querySelector(".card__rotate");
+
+// card__rotate.onclick = () => {
+//     card__rotate.style.transition = "2s";
+//     if (counterClicks % 2 === 0) {
+//         card.style.transform = "rotateY(180deg)";
+//     } else {
+//         card.style.transform = "rotateY(360deg)";
+//     }
+//     counterClicks++;
    
-};
+// };
 
-let counterClicksButton = 0;
-const button = document.querySelector(".button");
+// let counterClicksButton = 0;
+// const button = document.querySelector(".buttonShadow");
 
-button.onmousedown = () => {
-    button.style.transition = "1s";
-    // if(counterClicksButton % 2 === 0) {
-    button.style.boxShadow = `inset -31px -31px 43px rgba(255, 255, 255, 0.64), 
-        inset 26px 26px 48px rgba(13, 39, 80, 0.16),
-        -23px -23px 45px #FFF, 
-        28px 28px 50px rgba(13, 39, 80, 0.16)`;
-    // } else {
-    //     button.style.boxShadow = `-23px -23px 45px #FFF, 
-    //         28px 28px 50px rgba(13, 39, 80, 0.16)`;
-    // }
-    // counterClicksButton++;
-};
+// button.onmousedown = () => {
+//     button.style.transition = "1s";
+//     // if(counterClicksButton % 2 === 0) {
+//     button.style.boxShadow = `inset -31px -31px 43px rgba(255, 255, 255, 0.64), 
+//         inset 26px 26px 48px rgba(13, 39, 80, 0.16),
+//         -23px -23px 45px #FFF, 
+//         28px 28px 50px rgba(13, 39, 80, 0.16)`;
+//     // } else {
+//     //     button.style.boxShadow = `-23px -23px 45px #FFF, 
+//     //         28px 28px 50px rgba(13, 39, 80, 0.16)`;
+//     // }
+//     // counterClicksButton++;
+// };
 
-button.onmouseup = () => {
-    button.style.boxShadow = `-23px -23px 45px #FFF, 
-            28px 28px 50px rgba(13, 39, 80, 0.16)`;
-};
+// button.onmouseup = () => {
+//     setTimeout( () => {
+//         button.style.boxShadow = `-23px -23px 45px #FFF, 
+//             28px 28px 50px rgba(13, 39, 80, 0.16)`;
+//     }, 200 );   
+// };
 
-let programmers = [];
-//programmers.push( people.find(person => person.profession === "programmer") );
+// let programmers = [];
+// //programmers.push( people.find(person => person.profession === "programmer") );
 
-programmers = people.filter(person => person.profession == "programmer");
-console.log(programmers);
+// programmers = people.filter(person => person.profession == "programmer");
+// console.log(programmers);
 
-const delay = (wait = 1000) => {
-    const promise = new Promise( (resolve, reject) => {
-        setTimeout(() => {
-            resolve();
-        }, wait);
-    });
-    return promise;
-};
+// const delay = (wait = 1000) => {
+//     const promise = new Promise( (resolve, reject) => {
+//         setTimeout(() => {
+//             resolve();
+//         }, wait);
+//     });
+//     return promise;
+// };
 
-let changedPeople = new Array;
+// let changedPeople = new Array;
 
-console.log(people[1]);
-delay().then( () => {
-    console.log("after timeout");
-    changedPeople = people.map(person => {
-        return person.name = "Dear " + person.name;
-    });
-    console.log(changedPeople[0]);
-}); 
+// console.log(people[1]);
+// delay().then( () => {
+//     console.log("after timeout");
+//     changedPeople = people.map(person => {
+//         return person.name = "Dear " + person.name;
+//     });
+//     console.log(changedPeople[0]);
+// }); 
 
-async function asyncExample() {
-    await delay(3000);
-    console.log("I love you");
-}
+// async function asyncExample() {
+//     await delay(3000);
+//     console.log("I love you");
+// }
 
-asyncExample();
+// asyncExample();
 
-const mouse = {
-    type: "animal",
-    age: 3,
-};
+// const mouse = {
+//     type: "animal",
+//     age: 3,
+// };
 
-let user = {
-    userName: "Anna",
-    adult: undefined,
-    age: 24,
-    languages: ['C++', 'Ruby', 'HTML'],
+// let user = {
+//     userName: "Anna",
+//     adult: undefined,
+//     age: 24,
+//     languages: ['C++', 'Ruby', 'HTML'],
 
-    showUser(top = true, bottom = true) {
-        const keys = Object.keys(this);
-        if (top) {
-            console.log("-----------Start-------------");
-        }
-        keys.forEach( 
-            (key) => { console.log(`${key}: ${this[key]} `)} 
-        );
-        if (bottom) {
-            console.log("------------End--------------");
-        }
-    },
+//     showUser(top = true, bottom = true) {
+//         const keys = Object.keys(this);
+//         if (top) {
+//             console.log("-----------Start-------------");
+//         }
+//         keys.forEach( 
+//             (key) => { console.log(`${key}: ${this[key]} `)} 
+//         );
+//         if (bottom) {
+//             console.log("------------End--------------");
+//         }
+//     },
 
-    pushLanguage(language) {
-        user.languages.push(language);
-    },
-};
+//     pushLanguage(language) {
+//         user.languages.push(language);
+//     },
+// };
 
-//createUser();
-// const bound =  user.showUser.bind(mouse);
-// bound();
-user.pushLanguage("JavaScript");
+// //createUser();
+// // const bound =  user.showUser.bind(mouse);
+// // bound();
+// user.pushLanguage("JavaScript");
 
-let textToReverse = user.userName.split('');
+// let textToReverse = user.userName.split('');
 
-console.log(textToReverse.reverse().join(''));
+// console.log(textToReverse.reverse().join(''));
 
-user.showUser();
+// user.showUser();
 
-//showAllProperties();
-//showProperty( askQuestion("Choose property") );
+// //showAllProperties();
+// //showProperty( askQuestion("Choose property") );
 
-function showAllProperties() {
-    console.log("List of properties:");
+// function showAllProperties() {
+//     console.log("List of properties:");
 
-    for (let property in user) {
-        console.log(property);
-    }
-}
+//     for (let property in user) {
+//         console.log(property);
+//     }
+// }
 
 
-function showProperty(nameOfProperty) {
-    console.log(nameOfProperty + 
-        ": " + 
-        user[nameOfProperty]);
-}
+// function showProperty(nameOfProperty) {
+//     console.log(nameOfProperty + 
+//         ": " + 
+//         user[nameOfProperty]);
+// }
 
-function askQuestion(Question) {
-    return prompt(Question);
-}
+// function askQuestion(Question) {
+//     return prompt(Question);
+// }
 
-function getName() {
-    let userName = prompt("Could you tell your name, please?");
-    return userName ? userName : "No name";
-}
+// function getName() {
+//     let userName = prompt("Could you tell your name, please?");
+//     return userName ? userName : "No name";
+// }
 
-function checkAdult() {
-    return confirm("Are you more 21?");
-}
+// function checkAdult() {
+//     return confirm("Are you more 21?");
+// }
 
-function getAge() {
-    let age = prompt("How old are you?");
+// function getAge() {
+//     let age = prompt("How old are you?");
 
-    if (age == undefined) {
-        return "no age";
-    }
+//     if (age == undefined) {
+//         return "no age";
+//     }
     
-    if (+age >= 0 && +age <= 150) {
-        return +age;
-    }
+//     if (+age >= 0 && +age <= 150) {
+//         return +age;
+//     }
     
-    return "no age";
-}
+//     return "no age";
+// }
 
-function createUser() {
-    let showType = (nameOfVariable, variable) => {
-        console.log(`Type of ${nameOfVariable}: ${typeof(variable)}`)
-    };
+// function createUser() {
+//     let showType = (nameOfVariable, variable) => {
+//         console.log(`Type of ${nameOfVariable}: ${typeof(variable)}`)
+//     };
 
-    user.userName = getName();
-    //adult = checkAdult();
-    user.age = getAge();
-    showType("age", user.age);
-}
+//     user.userName = getName();
+//     //adult = checkAdult();
+//     user.age = getAge();
+//     showType("age", user.age);
+// }
 
